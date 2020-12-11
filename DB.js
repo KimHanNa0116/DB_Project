e5d81e38044e3395994a4bf118e4981ce2fd8e6d
KimHanNa0116 <ummane2006@naver.com>
2020-12-12 00:03:54 +0900

    [Modify] DB.js, router/main.js, views/FirstPage.html [Add] views/FindLocation.html

--- DB.js
+++ DB.js
@@ -8,6 +8,7 @@
 var express = require('express');
 var app = express();
 
+
 var bodyParser = require('body-parser'); 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
--- router/main.js
+++ router/main.js
@@ -3,8 +3,8 @@ module.exports = function(app)
      app.get('/',function(req,res){
         res.render('FirstPage.html')
      });
-     app.get('/about',function(req,res){
-        res.render('about.html');
+     app.get('/FindLocation',function(req,res){
+        res.render('FindLocation.html');
     });
 
 	app.post("/test1", function (req, res) {
@@ -14,7 +14,15 @@ module.exports = function(app)
   		console.log(req.body);
 
   		callback(req.body.email, req.body.pw)
-	});
+    });
+    app.post("/test2", function (req, res) {
+        var body = req.body;
+
+        console.log('in');
+        console.log(req.body);
+
+        callback2(req.body.loc);
+  });
 }
 
 function callback(id, password){
@@ -28,10 +36,8 @@ function callback(id, password){
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to the chinook database.');
+        console.log('Connected to the database.');
     })
-    // var id=1;
-    // var password=2;
     db.run(`INSERT INTO TABLE_USERINFO_DATA(ID, PW) VALUES('${id}', '${password}')`);
 
     db.close((err) => {
@@ -40,5 +46,35 @@ function callback(id, password){
         }
         console.log('Close the database connection.');
     });
+}
 
+function callback2(loc){
+
+    console.log(loc);
+    console.log("찾아보겠습니다");
+
+    //db열기
+    var sqlite3 = require("sqlite3").verbose();
+    let db = new sqlite3.Database('DATA.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the database.');
+    })
+ 
+    var list= new Array();
+    var i=0;
+    db.serialize(function() {
+        db.each("SELECT 관광지명 FROM TABLE_LOCATION_DATA WHERE 소재지지번주소 like '%" + loc +"%' UNION SELECT 관광지명 FROM TABLE_LOCATION_DATA WHERE 소재지도로명주소 like '%" + loc +"%'",
+        function(err, row){
+         console.log(row.관광지명);
+        });
+    });
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+   
 }
